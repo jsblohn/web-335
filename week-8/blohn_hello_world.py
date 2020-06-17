f0d2963f2cf2c4f360624e6eae76ae89cc366843
Janet Blohn <jblohn@my365.bellevue.edu>
2020-06-17 18:43:41 -0500

    Add Exercise 8.2

---
+++ week-8/blohn_hello_world.py
@@ -0,0 +1,13 @@
+#
+#==============================================================================
+# Title: blohn_hello_world.py
+# Author: Janet Blohn
+# Date:   06/17/2020
+# Modified Date: n/a
+# Description: Hello World Demo
+#==============================================================================
+print("Janet Blohn")
+print("Exercise 8.2")
+print("June 17, 2020")
+print()
+print("Hello World")
