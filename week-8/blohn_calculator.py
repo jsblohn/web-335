fe013f358ec4ce1b95a3322cd5824dcb9ffce984
Janet Blohn <jblohn@my365.bellevue.edu>
2020-06-17 22:17:31 -0500

    Add Exercise 8.3

--- week-8/blohn_calculator.py
+++ week-8/blohn_calculator.py
@@ -1,9 +1,9 @@
 #==============================================================================
-# Title: blohn_hello_world.py
+# Title: blohn_calculator.py
 # Author: Janet Blohn
 # Date:   06/17/2020
 # Modified Date: n/a
-# Description: Exercise 8.2 - Hello World Demo
+# Description: Exercise 8.3 Mathematical Functions
 # Date formatted from: https://phoenixnap.com/kb/get-current-date-time-python
 # and https://docs.python.org/2/library/datetime.html#strftime-and-strptime-behavior
 #==============================================================================
@@ -18,4 +18,16 @@ print(first_name + " " + last_name)
 print(exercise)
 print(today.strftime("%B %d, %Y"))
 print()
-print("Hello World")
+
+def add(number1, number2):
+  return number1 + number2
+
+def subtract(number1, number2):
+  return number1 - number2
+
+def divide(number1, number2):
+  return number1 / number2
+
+print(add(10, 30))
+print(subtract(60, 40))
+print(divide(100, 25))
